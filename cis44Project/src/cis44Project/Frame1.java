3e33f7dc59a65ef275efa6b0617ba37ea4711603
alfonsodelarosa4 <45708422+alfonsodelarosa4@users.noreply.github.com>
2020-04-20 00:11:35 -0700

    Updated Interface with Better Buttons and Titles

--- cis44Project/src/cis44Project/Frame1.java
+++ cis44Project/src/cis44Project/Frame1.java
@@ -2,19 +2,27 @@ package cis44Project;
 
 import java.awt.EventQueue;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Frame1 {
 
 	
 	private JFrame frame;
+	
+	private JComboBox<String> comboBoxSort;
 
 	/**
 	 * Launch the application.
@@ -33,11 +41,12 @@ public class Frame1 {
 	}
 	Connection connection = null;
 	private JTable table;
-	private JTextField textFieldIndex;
 	private JTextField textFieldName;
 	private JTextField textFieldImportance;
 	private JTextField textFieldCategory;
-	private JTextField textFieldDueDate;
+	private JTextField textFieldDueDateMonth;
+	private JTextField textFieldDueDateDay;
+	private JTextField textFieldDueDateYear;
 	
 	
 	/**
@@ -48,9 +57,60 @@ public class Frame1 {
 		connection = sqliteConnection.dbConnector();
 	}
 	
-	public static void updateTable()
+	public void refresh()
 	{
+		sortArray();
+		updateTable();
+	}
+	
+	public void sortArray()
+	{
+		switch((String) comboBoxSort.getSelectedItem())
+		{
+		case "Sort by Name":
+			//sort name algorithm
+			break;
+		case "Sort by Due Date":
+			//sort due date algorithm
+			break;
+		case "Sort by Category":
+			//sort category algorithm
+			break;
+		case "Sort by Importance":
+			//sort importance algorithm
+			break;
+		}
+	}
+	
+	public void updateTable()
+	{
+		/*
+		//delete contents of table
+		try {
+			String query = "delete * from Frame1";
+			
+			PreparedStatement pst = connection.prepareStatement(query);
+			
+			pst.execute();
+						
+			pst.close();
+			
+		} catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
 		
+		//add elements from bag array to table
+		
+		*/
+	}
+	
+	public void fillComboBox()
+	{
+		comboBoxSort.addItem("Sort by Name");
+		comboBoxSort.addItem("Sort by Due Date");
+		comboBoxSort.addItem("Sort by Category");
+		comboBoxSort.addItem("Sort by Importance");
 	}
 
 	/**
@@ -63,68 +123,135 @@ public class Frame1 {
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblTitle = new JLabel("Task Manager");
-		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblTitle.setBounds(10, 10, 151, 23);
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblTitle.setBounds(10, 10, 221, 37);
 		frame.getContentPane().add(lblTitle);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(373, 124, 700, 524);
+		scrollPane.setBounds(257, 125, 700, 524);
 		frame.getContentPane().add(scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
 		
-		JLabel lblIndex = new JLabel("Index");
-		lblIndex.setBounds(10, 139, 45, 13);
-		frame.getContentPane().add(lblIndex);
-		
 		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(10, 187, 45, 13);
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblName.setBounds(10, 145, 119, 13);
 		frame.getContentPane().add(lblName);
 		
 		JLabel lblImportance = new JLabel("Importance");
-		lblImportance.setBounds(10, 238, 99, 13);
+		lblImportance.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblImportance.setBounds(10, 197, 99, 26);
 		frame.getContentPane().add(lblImportance);
 		
 		JLabel lblCategory = new JLabel("Category");
-		lblCategory.setBounds(10, 289, 89, 13);
+		lblCategory.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblCategory.setBounds(10, 255, 99, 26);
 		frame.getContentPane().add(lblCategory);
 		
-		JLabel lblDueDate = new JLabel("Due Date");
-		lblDueDate.setBounds(10, 343, 45, 13);
-		frame.getContentPane().add(lblDueDate);
-		
-		textFieldIndex = new JTextField();
-		textFieldIndex.setBounds(20, 162, 96, 19);
-		frame.getContentPane().add(textFieldIndex);
-		textFieldIndex.setColumns(10);
+		JLabel lblDueDateMonth = new JLabel("Due Date (Month)");
+		lblDueDateMonth.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblDueDateMonth.setBounds(10, 320, 215, 13);
+		frame.getContentPane().add(lblDueDateMonth);
 		
 		textFieldName = new JTextField();
-		textFieldName.setBounds(20, 209, 96, 19);
+		textFieldName.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldName.setBounds(10, 168, 215, 30);
 		frame.getContentPane().add(textFieldName);
 		textFieldName.setColumns(10);
 		
 		textFieldImportance = new JTextField();
-		textFieldImportance.setBounds(20, 261, 96, 19);
+		textFieldImportance.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldImportance.setBounds(9, 222, 216, 30);
 		frame.getContentPane().add(textFieldImportance);
 		textFieldImportance.setColumns(10);
 		
 		textFieldCategory = new JTextField();
-		textFieldCategory.setBounds(20, 314, 96, 19);
+		textFieldCategory.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldCategory.setBounds(10, 280, 215, 30);
 		frame.getContentPane().add(textFieldCategory);
 		textFieldCategory.setColumns(10);
 		
-		textFieldDueDate = new JTextField();
-		textFieldDueDate.setBounds(20, 366, 96, 19);
-		frame.getContentPane().add(textFieldDueDate);
-		textFieldDueDate.setColumns(10);
+		textFieldDueDateMonth = new JTextField();
+		textFieldDueDateMonth.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldDueDateMonth.setBounds(10, 343, 215, 30);
+		frame.getContentPane().add(textFieldDueDateMonth);
+		textFieldDueDateMonth.setColumns(10);
 		
 		JButton btnAddTask = new JButton("Add Task");
-		btnAddTask.setBounds(10, 405, 85, 21);
+		btnAddTask.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//add element with a default constructor
+				//textFieldName.getText()
+				//textFieldImportance.getText()
+				//textFieldCategory.getText()
+				//textFieldDueDate.getText()
+				
+				refresh();
+				
+			}
+		});
+		btnAddTask.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnAddTask.setBounds(10, 566, 153, 21);
 		frame.getContentPane().add(btnAddTask);
 		
 		JButton btnRemoveTask = new JButton("Remove Task");
-		btnRemoveTask.setBounds(10, 436, 119, 21);
+		btnRemoveTask.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnRemoveTask.setBounds(10, 628, 153, 21);
 		frame.getContentPane().add(btnRemoveTask);
+		
+		JButton btnUpdateTask = new JButton("Update Task");
+		btnUpdateTask.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnUpdateTask.setBounds(10, 597, 153, 21);
+		frame.getContentPane().add(btnUpdateTask);
+		
+		JLabel lblNames = new JLabel("by Ali Altimimi and Alfonso De La Rosa");
+		lblNames.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNames.setBounds(10, 46, 360, 37);
+		frame.getContentPane().add(lblNames);
+		
+		JLabel lblTaskEditor = new JLabel("Task Editor");
+		lblTaskEditor.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblTaskEditor.setBounds(10, 119, 165, 20);
+		frame.getContentPane().add(lblTaskEditor);
+		
+		JLabel lblTaskTable = new JLabel("Task Table");
+		lblTaskTable.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblTaskTable.setBounds(260, 93, 153, 37);
+		frame.getContentPane().add(lblTaskTable);
+		
+		comboBoxSort = new JComboBox<String>();
+		comboBoxSort.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				refresh();
+			}
+		});
+		comboBoxSort.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		comboBoxSort.setBounds(434, 94, 184, 26);
+		frame.getContentPane().add(comboBoxSort);
+		
+		JLabel lblDueDateDay = new JLabel("Due Date (Day)");
+		lblDueDateDay.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblDueDateDay.setBounds(10, 378, 215, 26);
+		frame.getContentPane().add(lblDueDateDay);
+		
+		textFieldDueDateDay = new JTextField();
+		textFieldDueDateDay.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldDueDateDay.setColumns(10);
+		textFieldDueDateDay.setBounds(10, 406, 215, 30);
+		frame.getContentPane().add(textFieldDueDateDay);
+		
+		JLabel lblDueDateYear = new JLabel("Due Date (Year)");
+		lblDueDateYear.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblDueDateYear.setBounds(10, 439, 215, 26);
+		frame.getContentPane().add(lblDueDateYear);
+		
+		textFieldDueDateYear = new JTextField();
+		textFieldDueDateYear.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		textFieldDueDateYear.setColumns(10);
+		textFieldDueDateYear.setBounds(10, 467, 215, 30);
+		frame.getContentPane().add(textFieldDueDateYear);
+		
+		fillComboBox();
 	}
 }
